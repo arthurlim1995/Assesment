71bffe80efa19a696a92422812998248a9f65b09
arthur <arthurJJ1995@gmail.com>
2022-03-24 10:25:55 +0800

    Commit Changes.

--- Question 1.js
+++ Question 1.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { View, FlatList, Text, StyleSheet, TextInput, RefreshControl } from 'react-native';
-import ContactInfo from '/data.json';
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import { View, FlatList, Text, StyleSheet, TextInput, RefreshControl, TouchableOpacity, KeyboardAvoidingView, Platform, Button, Alert } from 'react-native';
 import { Avatar } from 'react-native-elements';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Conct from '/data.json';
 var detData = [];
+
+const ContactInfo = Conct
 
 class ContactList extends React.Component {
   constructor() {
     super();
     this.state = {
       contact: ContactInfo,
-      refreshing: true
+      refreshing: false
     };
   }
 
   contactName = (item) => {
-    const name = {item.firstName} + " " + {item.lastName};
-    return <Text style={styles.text} onPress={()=> this.props.navigation.navigate('Detail', {Contact: item})}>{name}</Text>;
+    return <Text style={styles.text} onPress={()=> this.props.navigation.navigate('Detail', {Contact: item})}>{item.firstName} {item.lastName}</Text>;
   }
 
   render() {
@@ -38,14 +39,27 @@ class ContactList extends React.Component {
 }
 
 class DetailScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fName: "",
+      lName: ""
+    };
+  }
+
+onSave = () => {
+      if (this.state.fName.length < 0) {
+        Alert.alert("Please Enter First Name");
+        return;
+      } else if (this.state.lName.length < 0) {
+        Alert.alert("Please Enter Last Name");
+        return;
+      }
+    }
 
   render() {
-    const ContactDet = this.props.navigation.state.params.Contact;
+    const ContactDet = this.props.route.params.Contact;
 
-    const refLastName = useRef();
-    const refEmail = useRef();
-    const refPhone = useRef();
-    
     let FirstNm = ""
     let LastNm = ""
     let Email = ""
@@ -58,113 +72,155 @@ class DetailScreen extends React.Component {
       Phone = ContactDet.phone;
     }
 
-    chkFirstName = (value) => {
-      if(!value.trim()) {
-        alert("Please Enter Name");
-        return;
-      }
-    }
-
-    chkLastName = (value) => {
-      if(!value.trim()) {
-        alert("Please Last Name");
-        return;
-      }
-    }
-
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={styles.container}
+        >
         <Avatar
         rounded
-        containerStyle={{ backgroundColor: '#ff8c00' }}
+        size = "medium"
+        containerStyle={{ backgroundColor: '#ff8c00', marginBottom: 25 }}
         />
         <Text style={styles.titleText}>More Information</Text>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.Infotext}>First Name: </Text>
-            <TextInput
-              style={styles.Infotext}
-              onChangeText={(value) => this.chkFirstName(value)}
-              returnKeyType="next"
-              blurOnSubmit={false}
-              onSubmitEditing={() => {
-                refLastName.current.focus();
-              }}
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+              <TextInput
+                editable={false}
+                style={[styles.Infotext, {paddingRight: 20}]}
+                disabledInputStyle={{opacity: 1, color: 'black' }}
+               >
+                First Name:
+               </TextInput>
+              <TextInput
+                style={styles.Infotext}
+                onChangeText={value => this.setState({ fName: value })}
+                returnKeyType="next"
+                blurOnSubmit={false}
+                onSubmitEditing={() => {
+                  this.LastName.focus();
+                }}
               >
-              {FirstNm}
-            </TextInput>
+                {FirstNm}
+              </TextInput>
           </View>
         </View>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.Infotext}>Last Name: </Text>
-            <TextInput
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput
+          editable={false}
+          style={[styles.Infotext, {paddingRight: 20}]}
+          disabledInputStyle={{opacity: 1}}
+          >
+            Last Name:
+          </TextInput>
+          <TextInput
               style={styles.Infotext}
-              onChangeText={(value) => this.chkLastName(value)}
+              onChangeText={(value) => this.setState({ lName: value })}
               returnKeyType="next"
               blurOnSubmit={false}
-              ref={refLastName}
+              ref={ref => {
+                this.LastName = ref;
+              }}
               onSubmitEditing={() => {
-                refEmail.current.focus();
+                this.Email.focus();
               }}
               >
               {LastNm}
-            </TextInput>
-          </View>
+          </TextInput>
         </View>
         <Text style={styles.titleText}>Sub Information</Text>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.Infotext}>Email: </Text>
-            <TextInput
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput
+            editable={false}
+            style={[styles.Infotext, {paddingRight: 20}]}
+            disabledInputStyle={{opacity: 1, color: 'black' }}
+            >
+              Email:
+          </TextInput>
+          <TextInput
               style={styles.Infotext}
+              onChangeText={(value) => this.setState({ emailDet: value })}
               returnKeyType="next"
               blurOnSubmit={false}
-              ref={refEmail}
+              ref={ref => {
+                this.Email = ref;
+              }}
               onSubmitEditing={() => {
-                refPhone.current.focus();
+                this.Phone.focus();
               }}
             >
               {Email}
-            </TextInput>
-          </View>
+          </TextInput>
         </View>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1 }}>
-            <Text style={styles.Infotext}>Phone: </Text>
-            <TextInput
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput
+            editable={false}
+            style={[styles.Infotext, {paddingRight: 20}]}
+            disabledInputStyle={{opacity: 1, color: 'black' }}
+            >
+              Phone:
+          </TextInput>
+          <TextInput
               style={styles.Infotext}
               returnKeyType="next"
-              ref={refPhone}
+              onChangeText={(value) => this.setState({ phoneDet: value })}
+              ref={ref => {
+                this.Phone = ref;
+              }}
               blurOnSubmit={false}
             >
               {Phone}
             </TextInput>
-          </View>
         </View>
-      </View>
+        <Button
+          style={styles.button}
+          title="Save"
+          onPress={this.onSave}
+        />
+      </KeyboardAvoidingView>
     );
   }
+  }
 
-export default createStackNavigator({
-  Contact: ContactList,
-  Detail: ContactDetail,
-});
+const Stack = createStackNavigator ();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Contact" component={ContactList} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   text: {
     fontSize:24
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   },
   titleText: {
     fontSize: 20,
     fontWeight: "bold"
   },
   Infotext: {
-    fontSize: 15
+    fontSize: 15,
+    borderColor: "#000000"
   },
 });
